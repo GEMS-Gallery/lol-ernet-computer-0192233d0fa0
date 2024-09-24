2badb4a9e1a43b66f285edd50309937555395260
sa-gems-github <sa-gems-github@dfinity.org>
2024-09-24 04:56:22 -0400

    0192233e-b59d-7005-8d32-427daee79412

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,10 @@ document.getElementById('jokeButton').addEventListener('click', () => {
     const jokes = [
         "Why did Mr. ICP bring a ladder to the bar? Because he heard the drinks were on the house!",
         "Mr. ICP doesn't do push-ups. He pushes the world down!",
-        "Why did Mr. ICP sit on his watch? Because he wanted to be on time!"
+        "Why did Mr. ICP sit on his watch? Because he wanted to be on time!",
+        "Why don't blockchain developers ever get lost? Because they always follow the chain!",
+        "Why was the blockchain developer broke? Because he lost his keys!",
+        "Why did the blockchain break up with the database? It couldn't handle the commitment!"
     ];
     const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
     document.getElementById('joke').innerText = randomJoke;
